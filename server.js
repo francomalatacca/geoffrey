a31dd848c1b5ee0c5c0b703f5c357e41d50e2db8
francomalatacca <francomalatacca@gmail.com>
2022-07-04 07:03:22 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    const { rawHeaders, httpVersion, method, socket, url } = request;
+    const { remoteAddress, remoteFamily } = socket;
+
+    const message = JSON.stringify({
+        timestamp: Date.now(),
+        rawHeaders,
+        httpVersion,
+        method,
+        remoteAddress,
+        remoteFamily,
+        url
+    });
+    logMessage(message);
+    response.end();
+});
+
+function logMessage(message, consoleOutputMessage = true, logFileName = 'server.log' ) {
+    if(consoleOutputMessage) {
+        console.log(message);
+    }
+
+    fs.appendFile(logFileName, message);
+}
+
+server.listen("0.0.0.0", "3000");
