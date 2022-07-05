46d07249defda350dcf29f3ba5b7d51276dd9fd6
francomalatacca <francomalatacca@gmail.com>
2022-07-05 18:43:02 +0200

    message parsing

--- routes/messages.js
+++ routes/messages.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
     console.log(req.body);
-    res.send(req.body);
+    const response = processIncomingObjects(req.body)
+    res.send(response);
 });
 
+function processIncomingObjects(object) {
+    if(object["object"] && object["object"] === "whatsapp_business_account") {
+        if(object["entry"] && object["entry"] instanceof Array) {
+            for (var el in object["entry"]) {
+                if( object["entry"]["el"]["field"] == "message") {
+                    for (var message in object["entry"]["el"]["value"]["messages"]) {
+                        text = ""
+                        if(object["entry"]["el"]["value"]["messages"]["type"] == "text") {
+                            text = text + " " + object["entry"]["el"]["value"]["messages"]["text"]
+                        }
+                    }
+                    return processMessage(text);
+                }
+            }
+        }
+    }
+}
+}
+
+function processMessage(entry) {
+    return entry == 'ciao' ? 'Hello' : '?';
+}
 module.exports = router;
